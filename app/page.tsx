573564d19713037a376276aa2ac9e2af9b81ab85
KevinLim62 <cheryiong62@gmail.com>
2024-04-24 15:19:50 +0800

    move books listing page to book route

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,13 @@ import Link from "next/link";
 export default async function Home() {
   const books = await getAllBooks();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-bold text-4xl text-center">Book Listing</h1>
-      {books.data.map((book: Book) => (
-        <div key={book.id} className="flex flex-col py-3">
-          <Link href={`/book/${book.id}`}>
-            <BookCard book={book} />
-          </Link>
-        </div>
-      ))}
+    <main className="flex min-h-screen flex-col items-center space-y-5 p-24">
+      <div className="text-4xl font-bold text-center">
+        Software Engineer Test
+      </div>
+      <Link href="/book" className="underline text-blue-400 text-lg">
+        Redirect to book page
+      </Link>
     </main>
   );
 }
